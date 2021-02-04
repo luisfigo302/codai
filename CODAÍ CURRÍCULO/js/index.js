449f7a26d274456ba38fe0ce8f55b4eb96adc986
João Silva <exemplo@seuemail.com.br>
2021-02-04 19:31:13 -0300

    Primeiro Commit

---
+++ CODAÍ CURRÍCULO/js/index.js
@@ -0,0 +1,34 @@
+let nome = "Paulo Miranda";
+var cargo = "Arquiteto de Sistemas";
+var nomehtml = document.getElementById("modifica-nome");
+var cargohtml = document.getElementById("modifica-cargo");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocaNomeNoHtml(nome){
+    nomehtml.innerHTML = nome; 
+}
+
+function trocaCargoHtml(cargo){
+    cargohtml.innerHTML = cargo;
+}
+
+function logarNome () {
+    console.log(nome +" "+"sua biba!!!")
+}
+
+function clickInProjetos(){
+    console.log("Clicou no botão Projetos!!");
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+
+function clickInSobre(){
+    console.log("Clicou no botão Sobre!!!");
+    texto1.style.display = "block";
+    texto2.style.display ="none";
+}
+
+colocaNomeNoHtml(nome);
+trocaCargoHtml(cargo);
+
